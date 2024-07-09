42a8acd14010c480534db8c0a019aba34e572f1a
oiat11 <fu.qia@northeastern.edu>
2024-07-09 14:09:55 -0700

    added the callback function

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
- import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import Header from './Components/Header';
 import Input from './Components/Input';
-import {  View, Text ,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   const appName = 'Summer 2024 class';
+  const [text, setText] = useState('');
+
+  function handleInputData(data) {
+    console.log('callback fn called with:', data);
+    setText(data);
+  }
 
   return (
     <View style={styles.container}>
-      <Header appName={appName} theme = 'dark'>
-        <Text>Lab2 Question 1: </Text>
-        <Text>onChange is used in React for web applications with standard HTML input elements. It triggers an event when the value of an input element changes. And we need to access the event.target.value to get the new value
-              onChangeText is used in React Native for mobile applications with React Native TextInput components. It directly provides the new value as a parameter to the handler.
-        <Text>Lab2 Question 2: </Text>    
-        <Text>add "secureTextEntry={true}" to TextInput</Text>
-        </Text>
+      <Header appName={appName} theme="dark">
+        <Text>children</Text>
       </Header>
-      <Input />
+      <Input inputHandler={handleInputData} />
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
--- Components/Input.js
+++ Components/Input.js
@@ -1,8 +1,7 @@
-import { View, Text, TextInput, Button } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native';
 import React, { useState, useEffect, useRef } from 'react';
 
-
-const Input = ({ autoFocus }) => {
+const Input = ({ inputHandler }) => {
   const [text, setText] = useState('');
   const [showThankYou, setShowThankYou] = useState(false);
   const textInputRef = useRef(null);
@@ -11,7 +10,7 @@ const Input = ({ autoFocus }) => {
     if (textInputRef.current) {
       textInputRef.current.focus();
     }
-  }, [autoFocus]);
+  }, []);
 
   const handleBlur = () => {
     setShowThankYou(true);
@@ -23,23 +22,24 @@ const Input = ({ autoFocus }) => {
 
   const handleConfirm = () => {
     console.log(`You typed: ${text}`);
-  }
+    inputHandler(text);
+  };
 
   return (
     <View>
-       <TextInput
-        style={{ height: 40}}
+      <TextInput
+        style={{ height: 40 }}
         placeholder="Type here to translate!"
         onChangeText={changedText => setText(changedText)}
         value={text}
         onBlur={handleBlur}
         onFocus={handleFocus}
-        />
-      <Text>User typed: {text}</Text>
+        ref={textInputRef}
+      />
       {showThankYou && <Text>Thank you</Text>}
-      <Button title='Confirm' onPress={handleConfirm} />
+      <Button title="Confirm" onPress={handleConfirm} />
     </View>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
