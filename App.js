850fcabb9d69600b1c2d082d8da3db76b9ddef90
oiat11 <fu.qia@northeastern.edu>
2024-07-09 15:01:23 -0700

    relative styling app.js component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import Header from './Components/Header';
 import Input from './Components/Input';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
@@ -16,15 +16,19 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
       <Header appName={appName} theme="dark">
         <Text>children</Text>
       </Header>
       <Input inputHandler={handleInputData} isModalVisible={modalVisible} />
-      <Text style={styles.textStyle}>{text}</Text>
-      <StatusBar style="auto" />
       <Button title="Add a goal" onPress={() => setModalVisible(true)} />
-    </View>
+      </View>
+      <View style={styles.bottomContainer}>
+      <Text style={styles.textStyle}>{text}</Text>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
@@ -32,11 +36,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
   textStyle: {
     fontSize: 20,
     color: 'red',
+  },
+  topContainer: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  bottomContainer: {
+    flex: 4,
+    backgroundColor: '#dcd',
+    alignItems: 'center',
   },
 });
