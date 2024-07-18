ecd8e4fea6bd23a6ab0cf44ca6452d47c8238071
oiat11 <fu.qia@northeastern.edu>
2024-07-18 13:37:13 -0700

    wrapped the goal items in a pressable

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,12 +1,14 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Pressable } from 'react-native';
 import React from 'react';
 
 const GoalItem = ({ goal, deleteHandler, navigation }) => {
   return (
     <View style={styles.textContainer}>
+      <Pressable style={styles.pressable} onPress={() => navigation.navigate('Details', { goalObj: goal })}>
       <Text style={styles.textStyle}>{goal.text}</Text>
       <Button color="black" title="X" onPress={() => deleteHandler(goal.id)} />
-      <Button color="black" title="i" onPress={() => navigation.navigate('Details', { goalObj: goal })} />
+  
+      </Pressable>
     </View>
   );
 };
@@ -25,8 +27,14 @@ const styles = StyleSheet.create({
       borderRadius: 5,
       alignSelf: 'center',
       justifyContent: 'center', 
-      alignItems: 'center', 
+      alignItems: 'center',
+      backgroundColor: '#f0f0f0',
     },
+    pressable: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    }
   });
 
 export default GoalItem;
