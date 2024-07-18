e99dd1985f5c4c8e166d23039ead6ae961464eda
oiat11 <fu.qia@northeastern.edu>
2024-07-18 13:42:29 -0700

    added android ripple color to pink

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -4,7 +4,11 @@ import React from 'react';
 const GoalItem = ({ goal, deleteHandler, navigation }) => {
   return (
     <View style={styles.textContainer}>
-      <Pressable style={styles.pressable} onPress={() => navigation.navigate('Details', { goalObj: goal })}>
+     <Pressable 
+        android_ripple={{ color: 'pink' }} 
+        style={styles.pressable} 
+        onPress={() => navigation.navigate('Details', { goalObj: goal })}
+      >
       <Text style={styles.textStyle}>{goal.text}</Text>
       <Button color="black" title="X" onPress={() => deleteHandler(goal.id)} />
   
