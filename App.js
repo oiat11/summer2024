b38d0fb412d14da5c098bbb183688617e19cb5c5
oiat11 <fu.qia@northeastern.edu>
2024-07-30 14:38:39 -0700

    added auth stack and use onAuthStateChanged in App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Home';
 import GoalDetails from './Components/GoalDetails';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './Firebase/FirebaseSetup';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,20 +15,44 @@ const commonScreenOptions = {
   headerTintColor: '#fff',
 };
 
+const AuthStack = (
+  <>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Signup" component={Signup} />
+  </>
+);
+
+const AppStack = (
+  <>
+    <Stack.Screen name="Home" component={Home} options={{ title: "All Goals" }} />
+    <Stack.Screen
+      name="Details"
+      component={GoalDetails}
+      options={({ route }) => ({
+        title: route.params.goalObj.text,
+      })}
+    />
+  </>
+);
+
 export default function App() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth,(user) => {
+      if (user) {
+        setIsUserAuthenticated(true);
+      }
+      else {
+        setIsUserAuthenticated(false);
+      }
+    })
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signup" screenOptions={commonScreenOptions}>
-        <Stack.Screen name="Home" component={Home} options={{title:"All Goals"}}/>
-        <Stack.Screen
-          name="Details"
-          component={GoalDetails}
-          options={({ route }) => ({
-            title: route.params.goalObj.text,
-          })}
-        />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator screenOptions={commonScreenOptions}>
+        {isUserAuthenticated ? AppStack : AuthStack}
       </Stack.Navigator>
     </NavigationContainer>
   );
