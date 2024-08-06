fc2f535b87d24ae026587842ff5c2eda54437015
oiat11 <fu.qia@northeastern.edu>
2024-08-06 14:20:41 -0700

    rendered image

--- Components/GoalDetails.js
+++ Components/GoalDetails.js
@@ -1,11 +1,14 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useEffect } from 'react';
 import { updateWarningStatus } from '../Firebase/firestoreHelper'; 
 import GoalUsers from './GoalUsers';
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from '../Firebase/FirebaseSetup'; 
 
 const GoalDetails = ({ navigation, route }) => {
   const [isWarning, setIsWarning] = useState(false);
   const { goalObj } = route.params;
+  const [imageUri, setImageUri] = useState('');
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -23,20 +26,48 @@ const GoalDetails = ({ navigation, route }) => {
     });
   }, [navigation, isWarning, goalObj.text]);
 
+  useEffect(() => {
+    async function fetchImageUri() {
+      if (goalObj.imageUri) {
+        try {
+          const url = await getDownloadURL(ref(storage, goalObj.imageUri));
+          setImageUri(url);
+        } catch (e) {
+          console.error('Error getting download URL:', e);
+        }
+      }
+    }
+
+    fetchImageUri();
+  }, [goalObj.imageUri]);
+
   return (
     <View style={styles.container}>
       <Text style={[styles.text, isWarning && styles.warningText]}>
         You are seeing the details of the goal with text: {goalObj.text} and id: {goalObj.id}
       </Text>
+      {imageUri ? <Image source={{ uri: imageUri }} style={styles.image} /> : null}
       <Button title="More Details" onPress={() => navigation.push('Details', { goalObj })} />
-      <GoalUsers id = {goalObj.id}/>
+      <GoalUsers id={goalObj.id} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+  },
   warningText: {
     color: 'red',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginVertical: 20,
   },
 });
 
