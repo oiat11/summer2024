41314e0e54ae13e53174aebc8ab02ebc38d72f57
oiat11 <fu.qia@northeastern.edu>
2024-08-08 14:02:05 -0700

    added Marker and pass location back to Profile

--- Components/Map.js
+++ Components/Map.js
@@ -1,8 +1,10 @@
-import { View, Text, Dimensions } from 'react-native';
-import React from 'react';
+import { View, Dimensions, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 
-const Map = () => {
+const Map = ({navigation}) => {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  
   const initialRegion = {
     latitude: 37.78825,
     longitude: -122.4324,
@@ -10,20 +12,50 @@ const Map = () => {
     longitudeDelta: 0.0421,
   };
 
+  const handleSave = () => {
+    console.log('Save location:', selectedLocation);
+    navigation.navigate("Profile", { selectedLocation: selectedLocation });
+  }
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <MapView
-        style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
+        style={styles.mapView}
         initialRegion={initialRegion}
+        onPress={(event) => {
+          setSelectedLocation({
+            latitude: event.nativeEvent.coordinate.latitude,
+            longitude: event.nativeEvent.coordinate.longitude,
+          });
+        }}
       >
-        <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title={"Marker Title"}
-          description={"Marker Description"}
-        />
+        {selectedLocation && (
+          <Marker coordinate={selectedLocation} />
+        )}
       </MapView>
+      {selectedLocation && (
+        <View style={styles.buttonContainer}>
+          <Button title="Confirm Selected Location" onPress={handleSave} />
+        </View>
+      )}
     </View>
   );
 };
 
 export default Map;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapView: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 50,
+    left: 20,
+    right: 20,
+  },
+});
